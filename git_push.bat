2adcd1765886864bf6ffb3eb795faf9b8d639091
Tushar-Wagdare <rahastugawa@gmail.com>
2025-02-03 15:19:06 +0530

    Modified : git_push.bat file modified...

--- git_push.bat
+++ git_push.bat
@@ -3,24 +3,29 @@ SETLOCAL
 
 cls
 
-echo ====================== git status ============================
+echo =========================== git status =================================
 git status
 pause
 
 echo/
+echo/
 set /p "commit_message=Enter your commit message: "
 
-echo ====================== git add -A ============================
+echo =========================== git add -A =================================
 git add -A
 echo/
-echo ====================== git commit -m "My Message" ============
+echo/
+echo =========================== git commit -m "My Message" =================
 git commit -m %commit_message%
 echo/
-echo ====================== git push ==============================
+echo/
+echo =========================== git push ===================================
 git push
 echo/
-echo ====================== git status ============================
+echo/
+echo =========================== git status =================================
 git status
+echo/
 echo/
 
 
