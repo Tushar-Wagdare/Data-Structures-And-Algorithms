f374d2006d2f4b68597db8e38ae14c702e404009
Tushar-Wagdare <rahastugawa@gmail.com>
2025-02-03 15:11:29 +0530

    Add : Modified git_push.bat file ...

--- git_push.bat
+++ git_push.bat
@@ -1,15 +1,20 @@
 SETLOCAL
 
 
+@echo  off
+echo ====================== git status ============================
 git status
-echo Please enter to continue .......
 pause
 
 set /p "commit_message=Enter your commit message: "
 
+echo ====================== git add -A ============================
 git add -A
+echo ====================== git commit -m %commit_message% ========
 git commit -m %commit_message%
+echo ====================== git push ==============================
 git push
+echo ====================== git status ============================
 git status
 
 
