aa120013a83866f5668a8a860cb7d0d3ce769dc2
Tushar-Wagdare <rahastugawa@gmail.com>
2025-02-03 15:15:32 +0530

    Modified : git_push.bat file modified...

--- git_push.bat
+++ git_push.bat
@@ -6,16 +6,21 @@ echo ====================== git status ============================
 git status
 pause
 
+echo/
 set /p "commit_message=Enter your commit message: "
 
 echo ====================== git add -A ============================
 git add -A
-echo ====================== git commit -m %commit_message% ========
+echo/
+echo ====================== git commit -m "My Message" ============
 git commit -m %commit_message%
+echo/
 echo ====================== git push ==============================
 git push
+echo/
 echo ====================== git status ============================
 git status
+echo/
 
 
 ENDLOCAL
