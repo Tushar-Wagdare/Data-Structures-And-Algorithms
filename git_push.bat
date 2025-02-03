515853f55d3035bb6bcd87a2d0406f40be41e8f9
Tushar-Wagdare <rahastugawa@gmail.com>
2025-02-03 15:05:44 +0530

    Add : added changes to the git_push.bat

--- git_push.bat
+++ git_push.bat
@@ -1,16 +1,16 @@
-@echo off
 SETLOCAL
+
+
+git status
+echo Please enter to continue .......
+pause
 
 set /p "commit_message=Enter your commit message: "
 
 git add -A
-
 git commit -m %commit_message%
-
 git push
-
 git status
 
-pause
 
 ENDLOCAL
